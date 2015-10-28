b47ee722387a83abfa79b0caf9368cef102204f3
Jan Mrázek <email@honzamrazek.cz>
2015-10-28 17:35:10 +0100

    Change repository structure, remove VS project

--- test_programs/final_tests/loops/sum01_true.c
+++
@@ -1,9 +0,0 @@
-#define a (2)
-extern int __VERIFIER_nondet_int();
-int main() { 
-  int i, n=__VERIFIER_nondet_int(), sn=0;
-  for(i=1; i<=n; i++) {
-    sn = sn + a;
-  }
-  __VERIFIER_assert(sn==n*a || sn == 0);
-}
--- test_programs/final_tests/prepare.sh
+++
@@ -1,7 +0,0 @@
-DIR=$1
-
-for i in $1/*.c; do
-    echo $i;
-    cat $i | sed 's/ERROR:/ERROR:assert(0);/g' > $i.backup.for.prepare;
-    mv $i.backup.for.prepare $i;
-done
